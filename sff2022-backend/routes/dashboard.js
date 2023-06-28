9598b006661b70f3513aaf27f1faffa878a5e758
vhugojc <vic_360.2000@hotmail.com>
2023-06-27 20:36:23 -0600

    BACK✨: Aplicacion de middlewares a dashboard endpoints

--- sff2022-backend/routes/dashboard.js
+++ sff2022-backend/routes/dashboard.js
@@ -22,13 +22,13 @@ const router = Router();
 // dashboard routes works with other controller file
 
 //team
-router.post('/team',postTeam);//create new team     [Dashboard]
-router.get('/team',getTeams);//get active teams     [Dashboard]
-router.put('/team',updateTeam);//update team        [Dashboard]
-router.delete('/team',deleteTeam);//set status team [Dashboard]
+router.post('/team',jwtSprAdminValidator, postTeam);//create new team     [Dashboard]
+router.get('/team',jwtSprAdminValidator, getTeams);//get active teams     [Dashboard]
+router.put('/team',jwtSprAdminValidator,updateTeam);//update team        [Dashboard]
+router.delete('/team',jwtSprAdminValidator,deleteTeam);//set status team [Dashboard]
 
 //seller
-router.post('/seller',[
+router.post('/seller',jwtSprAdminValidator,[
     check('email','El correo no es valido').isEmail(),
     check('name','El nombre es obligatorio').not().isEmpty(),
     check('lastname','El apellido es obligatorio').not().isEmpty(),
@@ -37,12 +37,12 @@ router.post('/seller',[
     fieldsValidation
 ],postSeller);               //create new seller     [Dashboard]
 
-router.get('/seller',getSellers);//get active sellers     [Dashboard]
-router.put('/seller',updateSellers);//update seller     [Dashboard]
-router.delete('/seller',deleteUser);//update seller     [Dashboard]
+router.get('/seller',jwtSprAdminValidator, getSellers);//get active sellers     [Dashboard]
+router.put('/seller',jwtSprAdminValidator, updateSellers);//update seller     [Dashboard]
+router.delete('/seller',jwtSprAdminValidator, deleteUser);//update seller     [Dashboard]
 
 //presales
-router.post('/presale',[
+router.post('/presale',jwtSprAdminValidator, [
     check('sellerId','El ID del equipo es obligatorio').not().isEmpty(),
     check('name','El nombre es obligatorio').not().isEmpty(),
     check('tortas','La cantidad de tortas es obligatoria').isNumeric(),
@@ -50,13 +50,13 @@ router.post('/presale',[
     fieldsValidation
 ],postPresale); //create new presale     [Dashboard]
 
-router.get('/presale',getPresale); //get active presale     [Dashboard]
-router.put('/presale',updatePresale); //create new presale     [Dashboard]
-router.delete('/presale',deletePresale); //create new presale     [Dashboard]
+router.get('/presale',jwtSprAdminValidator, getPresale); //get active presale     [Dashboard]
+router.put('/presale',jwtSprAdminValidator, updatePresale); //create new presale     [Dashboard]
+router.delete('/presale',jwtSprAdminValidator, deletePresale); //create new presale     [Dashboard]
 
 
 //login
-router.post('/admin/new',postSuperSeller); //remove aftercreate new admin
+// router.post('/admin/new', postSuperSeller); //remove aftercreate new admin
 router.post('/login', loginAdmin);
 router.get('/admin/me',jwtSprAdminValidator,validUser);
 
