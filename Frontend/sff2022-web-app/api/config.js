abf6e2cc6bb369fa5bbd3116aafcbcbf8721c4c0
vhugojc <vic_360.2000@hotmail.com>
2023-07-08 20:34:49 -0600

    WEB👽️: Update API url

--- Frontend/sff2022-web-app/api/config.js
+++ Frontend/sff2022-web-app/api/config.js
@@ -1,3 +1,10 @@
-export const BASEURL = 'http://localhost:5001/api';//server address
+let url = 'http://www.localhost:5001/api';
+if (typeof window !== 'undefined') {
+    if(window.location.host !== "localhost:3000" && window.location.host !== "www.localhost:3000"){
+        url = window.location.protocol + "//" + window.location.host+"/api";
+    }
+}
+export const BASEURL = url; //server address
+
 // develop: http://192.168.0.10:5000/api
 // testing: http://192.168.0.10:5000/api
